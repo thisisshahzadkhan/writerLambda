a2bbb515fe7eff326f2b0fcb09d53a33cda01f47
Shahzad Khan <thisisshahzadkhan@gmail.com>
2022-10-15 21:55:17 +0500

    finilized 1.0

--- user.js
+++ user.js
@@ -2,7 +2,7 @@ const dynamoose = require('dynamoose');
 // dynamoose config
 const ddb = new dynamoose.aws.ddb.DynamoDB({
     region: "us-east-1",
-    endpoint: "http://localhost:8000",
+    endpoint: "http://docker.for.mac.localhost:8000",
     apiVersion: 'latest',
     credentials: {
       accessKeyId: 'MY_ACCESS_KEY',
@@ -12,15 +12,13 @@ const ddb = new dynamoose.aws.ddb.DynamoDB({
   
 dynamoose.aws.ddb.set(ddb);
   
-dynamoose.aws.ddb.local();
-  
+dynamoose.aws.ddb.local('http://docker.for.mac.localhost:8000');
 
 const userSchema = new dynamoose.Schema({
     id: {type: String, hashKey: true},
     userId: {type: String, required: true},
     query: {type: String, required: true}
 });
-
 const user = dynamoose.model('usersLog', userSchema);
 
 module.exports = {user};
--- writeUserLambda.js
+++ writeUserLambda.js
@@ -3,12 +3,8 @@ const { v4: uuidv4 } = require('uuid');
 
 exports.handler = async (event) => {
     try {
-        // console.log('here i am', event);
-        // return {};
         if (event?.senderId === undefined || event?.query === undefined)
           throw 'Sender ID or Query should not be empty';
-        console.log('here ', event.senderId, event.query);
-        
         let newUser = new user({
           id: uuidv4(),
           userId: event.senderId+'',
@@ -18,7 +14,7 @@ exports.handler = async (event) => {
         await newUser.save();
         return newUser;
       } catch (error) {
+        console.log(error);
         return error;
       }
-    
-  }
+}
